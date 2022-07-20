d70f8f6b03360ab74b7568fdf1236684b7e134bb
Amirhoseinjafare <aj24091387@gmail.com>
2022-07-20 08:48:01 +0430

    my

---
+++ login.php
@@ -0,0 +1,12 @@
+
+<html>
+<body>
+
+<form action="" method="GET">
+ <label for="usename">Username:</label> 
+ <input type="text" name="usename"><br>
+  <label for="password">password</label>
+ <input type="password" name="password"><br>
+<input type="submit" name="login">
+</body>
+</html>
